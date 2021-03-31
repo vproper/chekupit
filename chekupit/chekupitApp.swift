47e43e391886562089e9a34e7980d0b5c548f056
vproper <bengboxer@gmail.com>
2021-03-31 11:36:01 +0300

    Initial Commit

---
+++ chekupit/chekupitApp.swift
@@ -0,0 +1,17 @@
+//
+//  chekupitApp.swift
+//  chekupit
+//
+//  Created by vantuzproper on 31.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct chekupitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
